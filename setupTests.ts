91b45fd37f222acd6925952b18b32e56b1322f1d
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-01-29 20:55:50 +0100

    chore: fix ts error in setupTest file

--- setupTests.ts
+++ setupTests.ts
@@ -1,11 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { describe, it, expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
-
-globalThis.describe = describe;
-globalThis.it = it;
-globalThis.expect = expect;
+(globalThis as any).describe = describe;
+(globalThis as any).it = it;
+(globalThis as any).expect = expect;
 
 afterEach(() => {
     cleanup();
