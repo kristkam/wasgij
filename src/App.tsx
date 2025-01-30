21cf578512fb899f420b719b6946d7a05e4b50ae
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-01-30 10:30:13 +0100

    style: grid and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./globalStyles";
 import { puzzles }  from "./puzzles";
 import { darkTheme } from "./defaultTheme";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
+  /* align-items: center;
+  justify-content: flex-start; */
+  /* flex-wrap: wrap; */
+  /* gap: 8px; */
   min-height: 100%;
 `;
 
@@ -42,25 +46,57 @@ const Image = styled.img`
 `;
 
 function App() {
-
+  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [items, setItems] = useState<{ id: string; [key: string]: any }[]>([]);
   const [searchFilter, setSearchFilter] = useState("");
 
   const filteredPuzzles = puzzles.filter((puzzle) => {
     return puzzle.title.toLocaleLowerCase().includes(searchFilter.toLowerCase())
   });
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "items"));
+
+      const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setItems(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
       <AppContainer>
-        <Title>Wasgij</Title>
-          <Input type="search" onChange={(e) => setSearchFilter(e.target.value)} placeholder="Search..." />
+        <div>
+          <Title>Wasgij</Title>
+          <Input 
+            type="search" 
+            onChange={(e) => setSearchFilter(e.target.value)} 
+            placeholder="Search..." 
+          />
+        </div>
+
+        <h3>Firebase data</h3>
+
+        {items.map((item, index) => (
+          <div key={index + item.name}>{item.name}</div>
+        ))}
+
+        <div style={{ display: "flex", flexWrap: "wrap", flex: "1 0", padding: "0 100px" }}>
           {filteredPuzzles.map((puzzle) => (
-            <div>
+            <div style={{ flex: "1 0 200px", gap: "10px" }}>
               <TextBox key={puzzle.title}>{puzzle.title}</TextBox>
               <Image src={puzzle.image_url} alt={puzzle.title} />
             </div>
           ))}
+        </div>
       </AppContainer>
     </ThemeProvider>
   );
