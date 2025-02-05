06eed75cd87680c7b7d8d1dd05705aa1dd18b715
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-02-05 11:50:38 +0100

    feat: added script to upload data to firebase

---
+++ src/uploadFirebaseData.ts
@@ -0,0 +1,19 @@
+import { db } from "./firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+import { puzzles } from "./puzzles";
+
+export const uploadFirebaseData = async () => {
+  try {
+    const collectionName = "puzzles";
+    
+    for (const key in puzzles) {
+      const puzzle = puzzles[key];
+
+      await addDoc(collection(db, collectionName), puzzle);
+    }
+
+    console.log("Data uploaded successfully!");
+  } catch (error) {
+    console.log('error saving data', error)
+  }
+};
