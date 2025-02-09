0aa2735e11cadd7d28aa98622dff332113b3fdb0
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-02-09 07:55:20 +0100

    style: new success colour

--- src/defaultTheme.ts
+++ src/defaultTheme.ts
@@ -66,11 +66,17 @@ export const darkTheme2 = {
       primary: "#8F6A9F", // interactive elements
       secondary: "#A67C75", // secondary buttons, hover states, or subtle UI details.
       highlight: "#C2B8A3", // use sparingly for notices, warnings, highlights, or key information that needs attention
+      success: "#6A9F6A"
     },
     text: {
       primary: "#E0E0E0", // main
       secondary: "#B8B8C6", // subtext, metadata, or descriptions
       disabled: "#7A7A8C", // disabled buttons, placeholder text, or UI hints
+    },
+    status: {
+      success: "#6A9F6A", // Green for checked items
+      error: "#BF6A6A",   // Example red for errors
+      warning: "#C2A36A"  // Example yellow for warnings
     }
   },
   typography: {
