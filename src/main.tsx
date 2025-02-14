4423558e0bf6663ebb5c86b47d048fbf2df7b48a
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-02-14 16:01:48 +0100

    chore: moved provider wrappers to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ThemeProvider } from 'styled-components'
-// import { lightTheme, darkTheme } from './defaultTheme.ts'
-// import { GlobalStyles } from './globalStyles.tsx'
+import { darkTheme2 } from './defaultTheme.ts';
+import { GlobalStyles } from './globalStyles.tsx';
+import { StyleSheetManager, ThemeProvider } from 'styled-components';
+import { shouldForwardProp } from './utils.ts';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={darkTheme2}>
+        <GlobalStyles />
+        <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+          <App />
+        </StyleSheetManager>
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
