b104cce5e79f95eb3ace82d718b419200571201c
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-02-15 09:59:24 +0100

    chore: eslint no-implicit-any set to warning

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      // "noImplicitAny": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
     },
   }
 );
