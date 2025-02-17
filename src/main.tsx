a7d744677d45df436b0ffbf5aa8cd7e39366efbc
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-02-17 22:21:30 +0100

    feat: login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { darkTheme2 } from './defaultTheme.ts';
 import { GlobalStyles } from './globalStyles.tsx';
 import { StyleSheetManager, ThemeProvider } from 'styled-components';
 import { shouldForwardProp } from './utils.ts';
+import Login from './components/Login.tsx';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider theme={darkTheme2}>
         <GlobalStyles />
         <StyleSheetManager shouldForwardProp={shouldForwardProp}>
-          <App />
+          <Login />
         </StyleSheetManager>
       </ThemeProvider>
     </QueryClientProvider>
