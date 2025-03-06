5c8ac1d6f4fec53dab7bb981e2bccaa641137b13
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-03-06 21:05:58 +0100

    chore: small fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
+import { scan } from 'react-scan';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { darkTheme2 } from './defaultTheme.ts';
+import { darkTheme } from './defaultTheme.ts';
 import { GlobalStyles } from './globalStyles.tsx';
 import { StyleSheetManager, ThemeProvider } from 'styled-components';
-import { shouldForwardProp } from './utils.ts';
-import Login from './components/Login.tsx';
+import { shouldForwardProp } from './utils/utils.ts';
+import { Login } from './components/';
+
+scan({ enabled: true });
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={darkTheme2}>
+      <ThemeProvider theme={darkTheme}>
         <GlobalStyles />
         <StyleSheetManager shouldForwardProp={shouldForwardProp}>
           <Login />
