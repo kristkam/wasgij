ebe99a2a4ce932516dc48afe303c18bf8eae69ab
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-03-06 21:02:08 +0100

    style: removed unused styling, preparing for light and dark themes

--- src/defaultTheme.ts
+++ src/defaultTheme.ts
@@ -1,31 +1,3 @@
-// export const defaultTheme = {
-//   colors: {
-//     primary: {
-//       purple: '#A259FF',
-//       yellow: '#FEC601',
-//       coral: '#FF6F61',
-//     },
-//     secondary: {
-//       teal: '#00B4D8',
-//       softBlue: '#90E0EF',
-//     },
-//     neutral: {
-//       offWhite: '#F5F5F5',
-//       charcoalGray: '#333333',
-//       lightGray: '#CCCCCC',
-//     },
-//     special: {
-//       correctGreen: '#4CAF50',
-//       warningAmber: '#FFC107',
-//       errorRed: '#F44336',
-//     },
-//   },
-//   typography: {
-//     fontSize: '16px',
-//     fontFamily: `'Arial', sans-serif`,
-//   }
-// };
-
 export const lightTheme = {
   colors: {
     background: "#F5F5F5",
@@ -53,7 +25,7 @@ export const lightTheme = {
   typography: { fontSize: "16px", fontFamily: "'Arial', sans-serif" },
 };
 
-export const darkTheme2 = {
+export const darkTheme = {
   colors: {
     background: {
       // primary: "#34344A",
@@ -87,34 +59,4 @@ export const darkTheme2 = {
     fontSize: '16px',
     fontFamily: `'Arial', sans-serif`,
   }
-};
-
-export const darkTheme = {
-  colors: {
-    background: "#303030",
-    surface: "#424242",
-    primary: {
-      purple: "#BB86FC",
-      yellow: "#FFD54F",
-      coral: "#FF8A80",
-    },
-    secondary: {
-      teal: "#64FFDA",
-      softBlue: "#82B1FF",
-    },
-    neutral: {
-      offWhite: "#E0E0E0",
-      charcoalGray: "#B3B3B3",
-      lightGray: "#757575",
-    },
-    special: {
-      correctGreen: "#81C784",
-      warningAmber: "#FFC107",
-      errorRed: "#FF5252",
-    },
-  },
-  typography: {
-    fontSize: "16px",
-    fontFamily: "'Arial', sans-serif",
-  },
 };
