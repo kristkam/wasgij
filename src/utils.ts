aef94695628be09eb1d9641f0a83c32e3551889b
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-03-06 21:03:03 +0100

    chore: deleting unused files

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-import isPropValid from "@emotion/is-prop-valid";
-
-export const shouldForwardProp = (propName: string, target: any) => {
-  return typeof target === "string" ? isPropValid(propName) : true;
-};
