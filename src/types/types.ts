491c3e5aaaf682caaf6ae807fb7b6f50c762395a
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-03-10 21:59:36 +0100

    chore: update types

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,3 +7,35 @@ export type Puzzle = {
   image_url: string;
   checked: boolean;
 };
+
+export type Themes = "light" | "dark";
+
+export type DefaultTheme = {
+  colors: {
+    background: {
+      surface: string;
+    };
+    text: {
+      primary: string;
+      secondary: string;
+      disabled: string;
+    };
+    accents: {
+      primary: string;
+      highlight: string;
+    };
+    status: {
+      success: string;
+      warning: string;
+      error: string;
+    },
+    overlay: {
+      primary: string;
+      secondary: string;
+    },
+  }
+  typography: {
+    fontSize: string;
+    fontFamily: string;
+  }
+};
