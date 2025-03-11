3edce0bd213b548aa225525a98b8fc6872aee407
k.kampenhoy <k.kampenhoy@sportradar.com>
2025-03-11 11:32:35 +0100

    chore: added props to checkmark svg for custom

--- src/svg/Checkmark.tsx
+++ src/svg/Checkmark.tsx
@@ -1,13 +1,21 @@
 import { motion } from 'motion/react'
 
-const CheckmarkSvg = () => {
+interface OwnProps {
+  size: {
+    width?: string;
+    height?: string;
+  },
+  color?: string;
+}
+
+const CheckmarkSvg = ({ size: { width = "32", height = "32" }, color = "#6A9F6A"}: OwnProps) => {
   return (
     <motion.svg
-      width="32"
-      height="32"
+      width={width}
+      height={height}
       viewBox="0 0 24 24" 
       fill="none" 
-      stroke="#6A9F6A"
+      stroke={color}
       strokeWidth="2"
     >
       <circle cx="12" cy="12" r="10"/>
